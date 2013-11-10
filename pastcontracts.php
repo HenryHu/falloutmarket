1529c22eced85b17d358ece5fd85125bdd716dea
Henry Hu <henry.hu.sh@gmail.com>
2013-11-09 19:55:22 -0500

    my contracts

--- pastcontracts.php
+++ pastcontracts.php
@@ -11,7 +11,7 @@ verify_session();
         <title>My Contracts</title>
     </head>
     <body>
-        <h1>My Current Contracts</h1>
+        <h1>My Past Contracts</h1>
         <table>
             <tr> <th> Product </th> <th> Price </th> <th> Quantity </th> <th> Begin </th> <th> End </th> <th> Sold </th> <th> Action </th> </tr>
 <?php
@@ -19,7 +19,7 @@ include_once 'conn.php';
 include_once 'util.php';
 
 $conn = db_connect();
-$stmt = db_bind_exe($conn, 'select goods.name, contracts.gid, contracts.cid, contracts.qty, TO_CHAR(contracts.begin, \'YYYY-MM-DD\') begin, TO_CHAR(contracts.end, \'YYYY-MM-DD\') end, contracts.price, sold from contracts, goods, (select sum(qty) sold, fulfill sold_cid from orders group by fulfill)  where contracts.userid = :userid and contracts.gid = goods.gid and contracts.cid = sold_cid and (contracts.end is null or contracts.end > TO_DATE(:now, \'YYYYMMDD\')) order by cid', array('userid' => session_userid(), 'now' => now()));
+$stmt = db_bind_exe($conn, 'select goods.name, contracts.gid, contracts.cid, contracts.qty, TO_CHAR(contracts.begin, \'YYYY-MM-DD\') begin, TO_CHAR(contracts.end, \'YYYY-MM-DD\') end, contracts.price, sold from contracts, goods, (select sum(qty) sold, fulfill sold_cid from orders group by fulfill)  where contracts.userid = :userid and contracts.gid = goods.gid and contracts.cid = sold_cid and (contracts.end is not null and contracts.end <= TO_DATE(:now, \'YYYYMMDD\')) order by cid', array('userid' => session_userid(), 'now' => now()));
 
 while ($ret = db_fetch_object($stmt)) {
     echo '<tr><td>' . $ret->NAME . '</td><td>' . $ret->PRICE . '</td>';
@@ -38,7 +38,7 @@ db_close($conn);
 ?>
 </table>
 
-<h3><a href='pastcontracts.php'>Past Contracts</a></h3>
+<h3><a href='mycontracts.php'>Current Contracts</a></h3>
 <h3><a href='dashboard.php'>Dashboard</a></h3>
     </body>
 </html>
