8f4e81a3419c189904bae77c18b313b8141f44e3
Henry Hu <henry.hu.sh@gmail.com>
2013-11-10 02:32:10 -0500

    add links

--- orderdetail.php
+++ orderdetail.php
@@ -22,12 +22,12 @@ $oid = get_arg('id');
 include_once 'conn.php';
 
 $conn = db_connect();
-$stmt = db_bind_exe($conn, 'select goods.name, orders.gid, orders.qty, TO_CHAR(placed, \'YYYY-MM-DD\') placed, fulfill, contracts.price, users.name seller, users.username seller_username, address, act_number from orders, goods, contracts, users, accounts, addresses where orders.userid = :userid and orders.gid = goods.gid and contracts.cid = fulfill and contracts.userid = users.userid and orders.oid = :oid and accounts.acctid = orders.payby and addresses.addrid = orders.shipto', array('userid' => session_userid(), 'oid' => $oid));
+$stmt = db_bind_exe($conn, 'select goods.name, orders.gid, orders.qty, TO_CHAR(placed, \'YYYY-MM-DD\') placed, fulfill, contracts.price, users.name seller, users.username seller_username, users.userid seller_uid, address, act_number from orders, goods, contracts, users, accounts, addresses where orders.userid = :userid and orders.gid = goods.gid and contracts.cid = fulfill and contracts.userid = users.userid and orders.oid = :oid and accounts.acctid = orders.payby and addresses.addrid = orders.shipto', array('userid' => session_userid(), 'oid' => $oid));
 
 while ($ret = db_fetch_object($stmt)) {
-    echo '<tr><th>Product</th><td>' . $ret->NAME . '</td></tr>';
-    echo '<tr><th>Seller</th><td>' . $ret->SELLER .
-        '(' . $ret->SELLER_USERNAME . ')</td></tr>';
+    echo '<tr><th>Product</th><td><a href="goodinfo.php?gid=' . $ret->GID . '">' . $ret->NAME . '</a></td></tr>';
+    echo '<tr><th>Seller</th><td><a href="sellerinfo.php?uid=' . $ret->SELLER_UID . '">' . $ret->SELLER .
+        '(' . $ret->SELLER_USERNAME . ')</a></td></tr>';
     echo '<tr><th>Price</th><td>' . $ret->PRICE . '</td></tr>';
     echo '<tr><th>Quantity</th><td>' . $ret->QTY . '</td></tr>';
     echo '<tr><th>Date</th><td>' . $ret->PLACED . '</td></tr>';
