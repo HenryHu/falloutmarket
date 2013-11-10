ea551dea258ec434c2eb0f2d626e762c5783288d
Henry Hu <henry.hu.sh@gmail.com>
2013-11-10 03:06:33 -0500

    change server

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');
 
 function db_connect() {
     include 'cred.php';
-	$db = "w4111b.cs.columbia.edu:1521/adb";
+	$db = "w4111c.cs.columbia.edu:1521/adb";
 	$conn = oci_connect($username, $password, $db);
     return $conn;
 }
