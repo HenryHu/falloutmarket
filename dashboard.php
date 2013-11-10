085e404702535780ad70a7836b656f50313c080a
Henry Hu <henry.hu.sh@gmail.com>
2013-11-09 19:56:55 -0500

    various functions

--- dashboard.php
+++ dashboard.php
@@ -15,27 +15,19 @@ verify_session();
         <h3>Welcome, <?php echo session_user(); ?> <a href='logout.php'>Logout</a> </h3>
         <h2>Personal Information</h2>
         <ul>
-            <li>
-                <a href='addressbook.php'>Addressbook</a>
-            </li>
-            <li>
-                <a href='myaccounts.php'>My accounts</a>
-            </li>
+            <li> <a href='addressbook.php'>Addressbook</a> </li>
+            <li> <a href='myaccounts.php'>My accounts</a> </li>
         </ul>
         <h2>Buyer</h2>
         <ul>
-            <li>
-                <a href='myorders.php'>My orders</a>
-            </li>
-            <li>
-                <a href='mycmts.php'>My comments</a>
-            </li>
+            <li><a href='buy.php'>I want to buy...</a></li>
+            <li> <a href='myorders.php'>My orders</a> </li>
+            <li> <a href='mycmts.php'>My comments</a> </li>
         </ul>
         <h2>Seller</h2>
         <ul>
-            <li>
-                <a href='mycontracts.php'>My contracts</a>
-            </li>
+            <li><a href='sell.php'>I want to sell...</a></li>
+            <li> <a href='mycontracts.php'>My contracts</a> </li>
         </ul>
         <p/>
     </body>
